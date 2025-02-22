98ad32cf42ff5fadcc3c29fffc36200dcb73d474
DanielHG2 <alfonsi.1920888@studenti.uniroma1.it>
2025-02-22 17:21:31 +0100

    Primo commit

---
+++ Lesson 1.py
@@ -0,0 +1,19 @@
+#ASSIGNMENT 1 2025
+## Python, R are interface lenguages. Translating to fortrun/C to backend to frontend.
+## Julia does things, exept matrixes, on his own. But still is intercative.
+## C and Fortrun are not interactive lenguages.
+## Assembler version of functions is going to be longer and heavier for floatings.
+## Python, R doesnt know how to do optimization, always going to produce longer code
+# ------> Julia codes are able to go 100times faster...
+
+## IN 2 WEEKS has to be submitted Assignment1 in py
+# producing a forects of macro series and 
+# add professor on git, and upload project possibly work on git   
+
+
+
+
+
+
+
+
